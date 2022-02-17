29f764a2ec7159e182b0e8e8eb519d2e892f60a5
Amir Khaki <amirkhaki995@gmail.com>
2022-02-17 16:43:16 +0000

    Sync products if deleted from govdeals

--- main.go
+++ main.go
@@ -11,6 +11,17 @@ import (
 var categoryID int = 57
 var perPage int = 25
 
+func contains(s []string, str string) bool {
+	for _, v := range s {
+		if v == str {
+			return true
+		}
+	}
+
+	return false
+}
+
+
 func unmarshal(jsn string) (map[string]interface{}, error) {
 	var rtrn map[string]interface{}
 	if err := json.Unmarshal([]byte(jsn), &rtrn); err != nil {
@@ -19,7 +30,7 @@ func unmarshal(jsn string) (map[string]interface{}, error) {
 	return rtrn, nil
 }
 func GetDBKey(id, acctid int) string {
-	return strconv.Itoa(id) + "|" + strconv.Itoa(acctid)
+	return strconv.Itoa(id) + "-" + strconv.Itoa(acctid)
 
 }
 func GetStartRowsFromUrl(uri string) (from, to int, err error) {
@@ -64,6 +75,7 @@ func GetProductID(uri string) (id, acctid int, err error) {
 }
 func main() {
 	db := GetDB()
+	sended := []string{}
 	s3Client, err := GetS3Client()
 	if err != nil {
 		fmt.Println("couldnt connect to s3\n", err)
@@ -156,6 +168,7 @@ func main() {
 					fmt.Println(err)
 					continue
 				}
+				sended = append(sended, dbKey)
 				fmt.Println("product updated: ", prdct.ID)
 				continue
 			}
@@ -172,11 +185,38 @@ func main() {
 			if id, ok := respMap["id"].(float64); ok {
 				idStr := strconv.Itoa(int(id))
 				fmt.Println("product sended: ", idStr)
-				Set(db, dbKey, idStr)
+				fmt.Println(Set(db, dbKey, idStr))
+				sended = append(sended, dbKey)
 				continue
 
 			}
 
 		}
 	}
+	deleted := []string{}
+	for k := range(db.Keys(nil)) {
+		if !contains(sended, k) {
+			dpidStr, err := Get(db, k)
+			if err != nil {
+				fmt.Println(err)
+				continue
+			}
+			dpid, err := strconv.Atoi(dpidStr)
+			if err != nil {
+				fmt.Println(err)
+				continue
+			}
+			if _, err = DeleteProduct(dpid); err != nil {
+				fmt.Println(err)
+				continue
+			}
+			Delete(db, k)
+			deleted = append(deleted, k)
+			fmt.Println("product deleted: ", dpid)
+			continue
+
+		}
+	}
+	fmt.Println("products sended: ", sended)
+	fmt.Println("products deleted: ", deleted)
 }
--- post.go
+++ post.go
@@ -7,6 +7,26 @@ import (
 	"net/http"
 	"os"
 )
+func DeleteProduct(pid int) (string, error){
+	wc_up_prdct_endpoint := os.Getenv("WC_URL") + "/wp-json/wc/v3/products/"
+	wc_up_prdct_endpoint += fmt.Sprintf("%d", pid)
+	wc_up_prdct_endpoint += "?consumer_key=" + os.Getenv("WP_KEY") + "&consumer_secret=" + os.Getenv("WP_SECRET")
+	req, err := http.NewRequest("DELETE", wc_up_prdct_endpoint, nil)
+	req.SetBasicAuth(os.Getenv("WC_KEY"), os.Getenv("WC_SECRET"))
+	req.Header.Set("Content-Type", "application/json; charset=UTF-8")
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return "", fmt.Errorf("Error during deleting product: %w", err)
+	}
+	defer resp.Body.Close()
+	response, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return "", fmt.Errorf("Error during reading delete response body: %w", err)
+	}
+	return string(response), nil
+}
+
 func UpdateProduct(p Product) (string, error){
 	wc_up_prdct_endpoint := os.Getenv("WC_URL") + "/wp-json/wc/v3/products/"
 	wc_up_prdct_endpoint += fmt.Sprintf("%d", p.ID)
