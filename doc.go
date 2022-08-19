62cce2109503a540a51c8b76d6f6c23961e60e1a
Amir Khaki <amirkhaki995@gmail.com>
2022-08-19 20:32:05 +0000

    List required env variables

---
+++ doc.go
@@ -0,0 +1,17 @@
+package main
+
+/**
+* Required env variables:
+* X_KEYWORDS //list of keywords that should be in product title
+* WC_DEVELOPING //if set, products wiil sent as pending
+* S3_BUCKET //s3 bucket name
+* S3_ENDPOINT //s3 endpoint without https://
+* S3_ACCESS
+* S3_SECRET
+* WC_URL //woocommerce url with https://
+* WP_KEY
+* WP_SECRET
+* WC_KEY
+* WC_SECRET
+*/
+
